b8327b7c55939e91a4f532dfdb8a423df79429f6
Friedrich Große <friedrich.grosse@gmail.com>
2019-05-19 18:38:38 +0200

    Restructure code

--- app.go
+++ app.go
@@ -1,6 +1,91 @@
 package main
 
-type App struct {
+import (
+	"image/color"
+	"log"
+	"time"
 
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"github.com/pkg/errors"
+)
+
+type App struct {
+	win *pixelgl.Window
+	render *Render
 }
 
+func NewApp(conf Config) (*App, error) {
+	today := time.Now()
+	year, month, day := today.Date()
+	checkIn := time.Date(year, month, day, 9, 30, 0, 0, time.Local)
+	checkOut := checkIn.Add(conf.WorkDuration).Add(conf.LunchDuration)
+	endOfDay := conf.DayEnd.Time(today)
+
+	win, err := newWindow(conf)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create window")
+	}
+
+	r, err := NewRender(conf, checkIn, checkOut, endOfDay)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &App{
+		win: win,
+		render: r,
+	}, nil
+}
+
+func (app *App) Run() {
+		fps := time.Tick(time.Second / 30)
+		last := time.Now()
+		for !app.win.Closed() {
+			dt := time.Since(last).Seconds()
+			last = time.Now()
+
+			app.win.Clear(color.White)
+			HandleInput(app.win, dt)
+			app.render.Draw(app.win)
+			app.win.Update()
+
+			<-fps
+		}
+}
+
+func newWindow(conf Config) (*pixelgl.Window, error) {
+	width := float64(conf.WindowWidth)
+	height := float64(conf.WindowHeight)
+
+	cfg := pixelgl.WindowConfig{
+		Title:       "Go Home",
+		Bounds:      pixel.R(0, 0, width, height),
+		VSync:       conf.VSync,
+		Undecorated: true,
+		Resizable:   false,
+		Floating:    true,
+		AutoIconify: true,
+	}
+
+	// TODO: we need GLFW 3.3 where we get the GLFW_TRANSPARENT_FRAMEBUFFER option
+	// See: https://www.glfw.org/docs/3.3/window_guide.html#window_hints_wnd
+
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	win.SetSmooth(true)
+
+	var displayWidth float64 = 1920 // TODO: make dynamic
+	pos := pixel.Vec{
+		X: displayWidth/2 - width/2,
+		Y: 29,
+	}
+
+	win.SetPos(pos)
+	win.Update()
+
+	return win, nil
+}
--- main.go
+++ main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"image/color"
 	"log"
 	"time"
 
-	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
-	"github.com/pkg/errors"
 )
 
 func main() {
-	pixelgl.Run(run)
-}
-
-func run() {
 	conf := Config{
 		WorkDuration:  8 * time.Hour,
 		LunchDuration: 1 * time.Hour,
@@ -25,69 +18,12 @@ func run() {
 		VSync:        true,
 	}
 
-	today := time.Now()
-	year, month, day := today.Date()
-	checkIn := time.Date(year, month, day, 9, 30, 0, 0, time.Local)
-	checkOut := checkIn.Add(conf.WorkDuration).Add(conf.LunchDuration)
-	endOfDay := conf.DayEnd.Time(today)
+	pixelgl.Run(func() {
+		app, err := NewApp(conf)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	win, err := NewWindow(conf)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	r, err := NewRender(conf, checkIn, checkOut, endOfDay)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fps := time.Tick(time.Second / 30)
-	last := time.Now()
-	for !win.Closed() {
-		dt := time.Since(last).Seconds()
-		last = time.Now()
-
-		win.Clear(color.White)
-		HandleInput(win, dt)
-		r.Draw(win)
-		win.Update()
-
-		<-fps
-	}
-}
-
-func NewWindow(conf Config) (*pixelgl.Window, error) {
-	width := float64(conf.WindowWidth)
-	height := float64(conf.WindowHeight)
-
-	cfg := pixelgl.WindowConfig{
-		Title:       "Go Home",
-		Bounds:      pixel.R(0, 0, width, height),
-		VSync:       conf.VSync,
-		Undecorated: true,
-		Resizable:   false,
-		Floating:    true,
-		AutoIconify: true,
-	}
-
-	// TODO: we need GLFW 3.3 where we get the GLFW_TRANSPARENT_FRAMEBUFFER option
-	// See: https://www.glfw.org/docs/3.3/window_guide.html#window_hints_wnd
-
-	win, err := pixelgl.NewWindow(cfg)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to create window")
-	}
-
-	win.SetSmooth(true)
-
-	var displayWidth float64 = 1920 // TODO: make dynamic
-	pos := pixel.Vec{
-		X: displayWidth/2 - width/2,
-		Y: 29,
-	}
-
-	win.SetPos(pos)
-	win.Update()
-
-	return win, nil
+		app.Run()
+	})
 }
